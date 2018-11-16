47ce5ae4ae729b47f84777f8e9210585ba1f7a3a
XXiao12 <shirley.xiao1996@gmail.com>
2018-11-15 21:01:19 -0500

    finished splashed page and set up routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Splash from './splash.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome!</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="app">
+          <Route exact path="/" component={Splash} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/splash.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Typed from 'typed.js';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import logo from './logo.svg';
+import './splash.css';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+class Splash extends Component {
+  componentDidMount() {
+    const options = {
+      strings: ["Hello, my name is Shirley."],
+      typeSpeed: 40,
+      backSpeed: 40
+    }
+    
+    this.typed = new Typed(this.el, options);
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="Splash">
+        <div className="Splash-header">
+          <p>Welcome!</p>
+          <div className="type-wrap">
+            <span
+              style={{ whiteSpace: 'pre' }}
+              ref={(el) => { this.el = el; }}
+            />
+          </div>
+          <p id="info">Click enter to find out more about me.</p>
+          <Button variant="contained" color="primary" className={classes.button}>
+            Enter
+          </Button>
+        </div>
+        <div className="Splash-footer">
+          <p>
+            Created with &nbsp;
+            <a
+              className="Splash-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            > React </a>
+            <img src={logo} className="Splash-logo" alt="logo" />
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+Splash.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Splash);
