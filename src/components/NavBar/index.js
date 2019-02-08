c53edfe1672a5ed5219abc4c648a53b613f57ced
XXiao12 <shirley.xiao1996@gmail.com>
2019-02-08 16:12:05 -0500

    changed navbar style

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,52 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom'
-import { withStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import githubMark from '../../img/GitHub-Mark/PNG/GitHubLight64px.png';
+// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+// import IconButton from '@material-ui/core/IconButton';
+// import githubMark from '../../img/GitHub-Mark/PNG/GitHubLight64px.png';
 import menuIcon from '../../img/menu/hamburger2x.png';
 
 import './styles.css';
 
-const styles = theme => ({
-    button: {
-      margin: theme.spacing.unit,
-    },
-    input: {
-      display: 'none',
-    },
-  });
-
-const NavBar = (props = {}) => {
-    const {
-        classes
-    } = props;
-
+const NavBar = () => {
     return (
         <div className="navbar">
-            <button className="navbar__menubutton">
-                <img className="navbar__menubutton__icon" src={menuIcon} alt="Menu Icon" />
-            </button>
             <div className="navbar__contents">
-                <p>Shirley Xiao</p>
-                <p>Resume</p>
-                <p>Design</p>
-                <p>Guestbook</p>
-                <IconButton
-                    target="_blank"
-                    href="https://github.com/XXiao12"
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}>
-                    <img className="githubLogo" src={githubMark} alt="Github Logo" />
-                </IconButton>
+                <h2>Shirley Xiao</h2>
+                <div className="navbar__contents__link">
+                    <Link className="link" to="/home">
+                        Resume
+                    </Link>
+                    <Link className="link" to="/home">
+                        Design
+                    </Link>
+                    <Link className="link" to="/home">
+                        Guestbook
+                    </Link>
+                </div>
+                <button className="navbar__menubutton topleft">
+                    <img className="navbar__buttonicon" src={menuIcon} alt="Menu Icon" />
+                </button>
             </div>
+            {/* <button className="navbar__socialbutton">
+                <img className="navbar__buttonicon" src={githubMark} alt="Github Logo" />
+            </button> */}
         </div>
     );
 };
 
-NavBar.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
+NavBar.PropTypes = {};
 
-export default withStyles(styles)(NavBar);
+export default NavBar;
