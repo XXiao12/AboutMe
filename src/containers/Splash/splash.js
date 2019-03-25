e8b3520d8af75f873359423bbb36923ae60bd390
XXiao12 <shirley.xiao1996@gmail.com>
2019-03-25 01:06:40 -0400

    edited splash screen

--- src/containers/Splash/splash.js
+++ src/containers/Splash/splash.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 import generateuuid from 'uuid';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 import logo from '../../img/React/logo.svg';
-import NavBar from '../../components/NavBar/index';
 import './splash.css';
 
 const styles = theme => ({
@@ -19,41 +18,42 @@ const styles = theme => ({
 });
 
 class Splash extends Component {
-  componentDidMount() {
-    const options = {
-      strings: ["My name is Shirley Xiao."],
-      typeSpeed: 40,
-      backSpeed: 40
-    }
+  // componentDidMount() {
+  //   const options = {
+  //     strings: ["my name is Shirley Xiao", "if you have come to learn more about me", "you have arrived at the correct url"],
+  //     typeSpeed: 40,
+  //     backSpeed: 40
+  //   }
     
-    this.typed = new Typed(this.el, options);
-  }
+  //   this.typed = new Typed(this.el, options);
+  // }
   render() {
     const { classes } = this.props;
     const homeLink = props => <Link to="/home" {...props} />
 
     return (
-      [
-      <NavBar key={generateuuid()} />,
       <div key={generateuuid()} className="Splash">
         <div className="Splash-overlay" />
         <div className="Splash-header">
           <div className="content">
-          <p>Welcome!</p>
-            <div className="type-wrap">
+          <p>Shirley Xiao</p>
+          <div className="nav">
+            <Button variant="outlined" href="https://github.com/XXiao12" color="inherit" className={classes.button}>
+              Github
+            </Button>
+            <Button variant="outlined" herf="https://www.linkedin.com/in/shirley-xiao-b72335a7/" color="inherit" className={classes.button}>
+              LinkedIn
+            </Button>
+            <Button variant="outlined" component={homeLink} color="primary" className={classes.button}>
+              Enter
+            </Button>
+          </div>
+            {/* <div className="type-wrap">
               <span
                 style={{ whiteSpace: 'pre' }}
                 ref={(el) => { this.el = el; }}
               />
-            </div>
-            <p id="info">Click enter to find out more about me.</p>
-            <Button
-              component={homeLink}
-              variant="contained"
-              color="primary"
-              className={classes.button}>
-              Enter
-            </Button>
+            </div> */}
           </div>
         </div>
         <div className="Splash-footer">
@@ -69,7 +69,6 @@ class Splash extends Component {
           </p>
         </div>
       </div>
-      ]
     );
   }
 }
