65af436052a91428d81a5cff331b1fb9ae2d1a46
XXiao12 <shirley.xiao1996@gmail.com>
2019-05-10 21:12:30 -0400

    changed Link to NavLink

--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import close from '../../img/menu/close.svg';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import './styles.css';
 
@@ -12,20 +12,22 @@ const MobileNav = (props = {}) => {
     } = props;
 
     return hidden ?
-        <div className="mobilenav__hidden"/> :
+        <div className="mobilenav__hidden" /> :
         <div className="mobilenav">
             <button onClick={onClick} ><img src={close} alt="close" /></button>
             <div>
-                <Link
+                <NavLink
+                    activeClassName="active"
                     className="links--themeblack"
                     to="/portfolio">
                     Portfolio
-            </Link>
-                <Link
+            </NavLink>
+                <NavLink
+                    activeClassName="active"
                     className="links--themeblack"
                     to="/about">
                     About
-            </Link>
+            </NavLink>
             </div>
         </div>
 };
