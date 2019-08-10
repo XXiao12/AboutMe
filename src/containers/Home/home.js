a215551d761f88b365af4933711ad4e289e4244c
XXiao12 <shirley.xiao1996@gmail.com>
2019-08-09 22:05:14 -0400

    added a explination for the different buttons

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -4,6 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ProjectButton from '../../components/ProjectButton';
 import WrapperComponent from '../../components/Wrapper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import { faDribbble } from '@fortawesome/free-brands-svg-icons';
 
 import './styles.css';
 
@@ -24,6 +27,11 @@ class Home extends Component {
       <WrapperComponent>
         <div className="home">
           <div>
+            <p className="home__old"><span><FontAwesomeIcon icon={faClock} /> <b>Old Work</b></span><br />Art done in my spare time, design projects/assignments, jobs, and from school club activities. The works were completed from highschool and throughout my time in university.</p>
+            <p className="home__dribbble"><span><FontAwesomeIcon icon={faDribbble} /> <b>Dribbble</b></span><br />UI challenges from a website called <a href="https://www.dailyui.co/"> Daily UI</a>. I try to complete some in my spare time.</p>
+            <p className="home__private"><span><FontAwesomeIcon icon={faUserShield} /> Private Projects</span><br />There are 2 ongoing projects that are not allowed to be displayed on the internet. I would gladly show these projects in an in-person meeting or interview.</p>
+          </div>
+          <div className="home__button">
             <Button
               variant="outlined"
               href="https://shirley.portfoliobox.net/graphicdesign"
@@ -41,14 +49,6 @@ class Home extends Component {
               color="inherit"
               className={classes.button}>
               Dribbble
-            </Button>
-            <Button
-              variant="outlined"
-              href="https://shirleyxiao1996.wixsite.com/mysite"
-              rel="noopener noreferrer"
-              color="inherit"
-              className={classes.button}>
-              Private Projects
             </Button>
           </div>
           <div className="projects">
