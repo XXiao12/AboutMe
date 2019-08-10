a11e1a96f712b97706059fd274ab4dfef45e4acf
XXiao12 <shirley.xiao1996@gmail.com>
2019-08-09 23:21:21 -0400

    changed style of other design sites

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -12,7 +12,30 @@ import './styles.css';
 
 const styles = theme => ({
   button: {
+    position: 'absolute',
+    bottom: 10,
+    marginLeft: 'auto',
+    marginRight: 'auto',
     margin: theme.spacing.unit,
+    fontSize: '12px',
+    fontFamily: 'helvetica',
+    textTransform: 'none',
+    fontWeight: 600,
+    backgroundColor: 'white',
+    color: '#6b0707'
+  },
+  buttond: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    position: 'absolute',
+    bottom: 10,
+    margin: theme.spacing.unit,
+    fontSize: '12px',
+    fontFamily: 'helvetica',
+    textTransform: 'none',
+    fontWeight: 600,
+    backgroundColor: 'white',
+    color: '#ea4c89'
   },
   input: {
     display: 'none',
@@ -26,30 +49,28 @@ class Home extends Component {
     return (
       <WrapperComponent>
         <div className="home">
-          <div>
-            <p className="home__old"><span><FontAwesomeIcon icon={faClock} /> <b>Old Work</b></span><br />Art done in my spare time, design projects/assignments, jobs, and from school club activities. The works were completed from highschool and throughout my time in university.</p>
-            <p className="home__dribbble"><span><FontAwesomeIcon icon={faDribbble} /> <b>Dribbble</b></span><br />UI challenges from a website called <a href="https://www.dailyui.co/"> Daily UI</a>. I try to complete some in my spare time.</p>
-            <p className="home__private"><span><FontAwesomeIcon icon={faUserShield} /> Private Projects</span><br />There are 2 ongoing projects that are not allowed to be displayed on the internet. I would gladly show these projects in an in-person meeting or interview.</p>
-          </div>
-          <div className="home__button">
-            <Button
-              variant="outlined"
-              href="https://shirley.portfoliobox.net/graphicdesign"
-              target="_blank"
-              rel="noopener noreferrer"
-              color="inherit"
-              className={classes.button}>
-              Old Work
-            </Button>
-            <Button
-              variant="outlined"
-              href="https://dribbble.com/shirley_x"
-              target="_blank"
-              rel="noopener noreferrer"
-              color="inherit"
-              className={classes.button}>
-              Dribbble
-            </Button>
+          <div className="home__container">
+            <div className="home__container--content">
+              <p className="home__old"><span className="color"><FontAwesomeIcon icon={faClock} /> <b>Old Work</b></span>Art done in my spare time, design projects/assignments, jobs, and from school club activities. The works were completed from highschool and throughout my time in university.
+              <Button
+                  href="https://shirley.portfoliobox.net/graphicdesign"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                  className={classes.button}>
+                  See old work
+                </Button></p>
+              <p className="home__dribbble"><span className="color"><FontAwesomeIcon icon={faDribbble} /> <b>Dribbble</b></span><span>UI challenges from a website called <a id="link" href="https://www.dailyui.co/"> Daily UI</a>. I try to complete some in my spare time.</span>
+                <Button
+                  href="https://dribbble.com/shirley_x"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                  className={classes.buttond}>
+                  See Dribbble work
+            </Button></p>
+              <p className="home__private"><span className="color"><FontAwesomeIcon icon={faUserShield} /> Private Projects</span>There are 2 ongoing projects that are not allowed to be displayed on the internet. I would gladly show these projects in an in-person meeting or interview.</p>
+            </div>
           </div>
           <div className="projects">
             <ProjectButton
