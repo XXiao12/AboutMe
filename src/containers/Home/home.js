a80bf954b0ea7856b3f42a31e068ce07cdbaeb34
Shirley Xiao <shirley.xiao@qochealth.com>
2019-11-22 00:21:55 -0500

    edited projectbutton styles

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,125 +1,157 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import ProjectButton from '../../components/ProjectButton';
-import WrapperComponent from '../../components/Wrapper';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock, faUserShield } from '@fortawesome/free-solid-svg-icons';
-import { faDribbble } from '@fortawesome/free-brands-svg-icons';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import ProjectButton from "../../components/ProjectButton";
+import WrapperComponent from "../../components/Wrapper";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faUserShield } from "@fortawesome/free-solid-svg-icons";
+import { faDribbble } from "@fortawesome/free-brands-svg-icons";
 
-import './styles.css';
+import "./styles.css";
 
 const styles = theme => ({
   button: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 10,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    marginLeft: "auto",
+    marginRight: "auto",
     margin: theme.spacing.unit,
-    fontSize: '12px',
-    fontFamily: 'helvetica',
-    textTransform: 'none',
+    fontSize: "12px",
+    fontFamily: "helvetica",
+    textTransform: "none",
     fontWeight: 600,
-    backgroundColor: '#d8d7d7',
-    color: 'black'
+    backgroundColor: "#d8d7d7",
+    color: "black"
   },
   buttond: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    position: 'absolute',
+    marginLeft: "auto",
+    marginRight: "auto",
+    position: "absolute",
     bottom: 10,
     margin: theme.spacing.unit,
-    fontSize: '12px',
-    fontFamily: 'helvetica',
-    textTransform: 'none',
+    fontSize: "12px",
+    fontFamily: "helvetica",
+    textTransform: "none",
     fontWeight: 600,
-    backgroundColor: '#ffd6e8',
-    color: '#da1460',
+    backgroundColor: "#ffd6e8",
+    color: "#da1460",
     "&:hover": {
       //you want this to be the same as the backgroundColor above
       backgroundColor: "#ffe3ef"
     }
   },
   buttonp: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    position: 'absolute',
+    marginLeft: "auto",
+    marginRight: "auto",
+    position: "absolute",
     bottom: 10,
     margin: theme.spacing.unit,
-    fontSize: '12px',
-    fontFamily: 'helvetica',
-    textTransform: 'none',
+    fontSize: "12px",
+    fontFamily: "helvetica",
+    textTransform: "none",
     fontWeight: 600,
-    backgroundColor: '#4a9ff3',
-    color: 'white',
+    backgroundColor: "#4a9ff3",
+    color: "white",
     "&:hover": {
       //you want this to be the same as the backgroundColor above
       backgroundColor: "#82c1ff"
     }
   },
   input: {
-    display: 'none',
-  },
+    display: "none"
+  }
 });
 
 class Home extends Component {
   render() {
     const { classes } = this.props;
-    const Process = props => <Link to="/process" {...props} />
+    const Process = props => <Link to="/process" {...props} />;
 
     return (
       <WrapperComponent>
         <div className="home">
           <div className="home__container">
             <div className="home__container--content">
-              <p className="home__private"><span className="color"><FontAwesomeIcon icon={faUserShield} /> Private Projects</span>There are ongoing projects that are not allowed to be displayed on the internet. I would gladly show these projects in an in-person meeting or interview.<Button
+              <p className="home__private">
+                <span className="color">
+                  <FontAwesomeIcon icon={faUserShield} /> Private Projects
+                </span>
+                There are ongoing projects that are not allowed to be displayed
+                on the internet. I would gladly show these projects in an
+                in-person meeting or interview.
+                <Button
                   component={Process}
                   rel="noopener noreferrer"
                   color="inherit"
-                  className={classes.buttonp}>
+                  className={classes.buttonp}
+                >
                   See design process
-            </Button></p>
-              <p className="home__dribbble"><span className="color"><FontAwesomeIcon icon={faDribbble} /> <b>Dribbble</b></span><span>UI challenges from a website called <a id="link" href="https://www.dailyui.co/"> Daily UI</a>. I try to complete some in my spare time.</span>
+                </Button>
+              </p>
+              <p className="home__dribbble">
+                <span className="color">
+                  <FontAwesomeIcon icon={faDribbble} /> <b>Dribbble</b>
+                </span>
+                <span>
+                  UI challenges from a website called{" "}
+                  <a id="link" href="https://www.dailyui.co/">
+                    {" "}
+                    Daily UI
+                  </a>
+                  . I try to complete some in my spare time.
+                </span>
                 <Button
                   href="https://dribbble.com/shirley_x"
                   target="_blank"
                   rel="noopener noreferrer"
                   color="inherit"
-                  className={classes.buttond}>
+                  className={classes.buttond}
+                >
                   See Dribbble work
-            </Button></p>
-              <p className="home__old"><span className="color"><FontAwesomeIcon icon={faClock} /> <b>Old Work</b></span>Art done in my spare time, design projects/assignments, jobs, and from school club activities. The works were completed from highschool and throughout my time in university.
-              <Button
+                </Button>
+              </p>
+              <p className="home__old">
+                <span className="color">
+                  <FontAwesomeIcon icon={faClock} /> <b>Old Work</b>
+                </span>
+                Art done in my spare time, design projects/assignments, jobs,
+                and from school club activities. The works were completed from
+                highschool and throughout my time in university.
+                <Button
                   varient="contained"
                   href="https://shirley.portfoliobox.net/graphicdesign"
                   target="_blank"
                   rel="noopener noreferrer"
                   color="inherit"
-                  className={classes.button}>
+                  className={classes.button}
+                >
                   See old work
-                </Button></p>
+                </Button>
+              </p>
             </div>
           </div>
 
-          <h1>Recent Works</h1>
-          <div className="projects">
-            <ProjectButton
-              name="The King in Yellow"
-              descriptionTag="CST"
-              company="Colliding Scope Theatre"
-              link="/colliding_scopes_theatre"
-            />
+          <div className="recent__projects">
+            <h1>Recent Works</h1>
+            <div className="projects">
+              <ProjectButton
+                name="The King in Yellow"
+                descriptionTag="CST"
+                company="Colliding Scope Theatre"
+                link="/colliding_scopes_theatre"
+              />
+            </div>
           </div>
-        </div >
+        </div>
       </WrapperComponent>
     );
   }
 }
 
 Home.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Home);
